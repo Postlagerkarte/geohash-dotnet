13fef0be9e81173bc7aa90c1faa0b82ad9ffd160
Postlagerkarte <hannes.pavelka@gmail.com>
2019-10-20 21:38:49 +0200

    add comments to enum

--- src/Direction.cs
+++ src/Direction.cs
@@ -6,13 +6,37 @@ namespace Geohash
 {
     public enum Direction
     {
+        /// <summary>
+        /// North
+        /// </summary>
         North = 0,
+        /// <summary>
+        /// North East
+        /// </summary>
         NorthEast = 1,
+        /// <summary>
+        /// East
+        /// </summary>
         East = 2,
+        /// <summary>
+        /// South East
+        /// </summary>
         SouthEast = 3,
+        /// <summary>
+        /// South
+        /// </summary>
         South = 4,
+        /// <summary>
+        /// South West
+        /// </summary>
         SouthWest = 5,
+        /// <summary>
+        /// West
+        /// </summary>
         West = 6,
+        /// <summary>
+        /// North West
+        /// </summary>
         NorthWest = 7
     }
 }
